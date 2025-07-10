4f58b67918d256a05d28ed65987727e4dac6e3d0
samuel88 <samuelgide88@gmail.com>
2025-07-10 19:27:19 +0300

    index.html update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <main className="max-w-7xl relative flex justify-center items-center flex-col">
+      <main className="max-w-7xl m-auto relative flex justify-center items-center flex-col">
       <div className='w-full fixed top-0 left-0 z-10 bg-green-500 py-3 shadow-green-800 shadow-xl'>
         <h2 className='ml-5 text-center text-shadow-green-800 text-shadow-lg'>Foodie Delight</h2>
       </div>
